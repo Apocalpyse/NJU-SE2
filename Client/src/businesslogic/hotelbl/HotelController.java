1af6c44c807ee3e24f30314a76392d32b4bb7915
chenjin163 <13951723565@163.com>
2016-11-27 18:17:11 +0800

    Update HotelController.java

--- Client/src/businesslogic/hotelbl/HotelController.java
+++ Client/src/businesslogic/hotelbl/HotelController.java
@@ -1,7 +1,37 @@
 package businesslogic.hotelbl;
 
-/**
- * Created by 常德隆 on 2016/11/19.
- */
+import java.util.ArrayList;
+
+import po.OrderState;
+import vo.HotelVO;
+
 public class HotelController {
+	HotelBL hbl;
+	public HotelController(){
+		hbl=new HotelBL();
+	}
+	public HotelVO getHotel(long id){
+		return hbl.getHotel(id);
+	}
+	public boolean changeHotel(HotelVO hv){
+		return hbl.changeHotel(hv);
+	}
+	public boolean changePassWord(long id,String pw){
+		return hbl.changePassWord(id, pw);
+	}
+	public boolean addEvaluation(long id,ArrayList evaluation){
+		return hbl.addEvaluation(id, evaluation);
+	}
+    public boolean changeOraderState(long id,OrderState os){
+    	return hbl.changeOraderState(id, os);
+    }
+    public boolean setPromotionOne(double[] numberOfRoom,double[] discount){
+    	return hbl.setPromotionOne(numberOfRoom, discount);
+    }
+    public boolean setPromotionTwo(double discount){
+    	return hbl.setPromotionTwo(discount);
+    }
+    public boolean setPromotionThree(String beginTime,String endTime){
+    	return hbl.setPromotionThree(beginTime, endTime);
+    }
 }
