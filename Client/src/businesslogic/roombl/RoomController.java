2bf5825c89090f6936e8156bca49b7c89594025d
chenjin163 <13951723565@163.com>
2016-11-27 18:18:16 +0800

    Update RoomController.java

--- Client/src/businesslogic/roombl/RoomController.java
+++ Client/src/businesslogic/roombl/RoomController.java
@@ -1,7 +1,18 @@
 package businesslogic.roombl;
 
-/**
- * Created by 常德隆 on 2016/11/20.
- */
+import dataservice.roomdataservice.RoomDataService;
+import po.RoomPO;
+import vo.RoomVO;
+
 public class RoomController {
+	RoomBL rbl;
+	public RoomController(){
+		rbl=new RoomBL();
+	}
+	public RoomVO getRoom(String roomStyle){
+		return rbl.getRoom(roomStyle);
+	}
+	public boolean changeRoom(RoomVO rv){
+		return rbl.changeRoom(rv);
+	}
 }
