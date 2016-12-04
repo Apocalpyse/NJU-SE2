cc7a28173b0b389b37880d20a8fdffe703027cc0
chenjin163 <13951723565@163.com>
2016-12-04 21:52:06 +0800

    Update HotelPO.java

--- Client/src/po/HotelPO.java
+++ Client/src/po/HotelPO.java
@@ -1,78 +1,87 @@
 package po;
-import java.io.Serializable;
 import java.util.ArrayList;
-/**
- * Created by 常德隆 on 2016/11/19.
- */
-public class HotelPO implements Serializable{
-    private String password;//密码
-    private String hotelName;//酒店名称
-    private String tradeArea;//所属商圈
-    private String hotelLocation;//酒店地址
-    private int[] hotelPhone;//酒店联系电话
-    private int stars;//酒店星级
-    private String instruction;//酒店简介
-    private ArrayList<Evaluation> eval;//酒店评价
-    private String cooperatateCompany;//酒店合作企业
 
-    public HotelPO(){}
-
-    public HotelPO(String hn,String ta,String hl,int[] hp,int st,String in,Evaluation eva){
-        hotelName=hn;
-        tradeArea=ta;
-        hotelLocation=hl;
-        hotelPhone=hp;
-        stars=st;
-        instruction=in;
-        eval.add(eva);
-    }
-    public void setPassword(String pw){
-        password=pw;
-    }
-    public void setHotelName(String hn){
-        hotelName=hn;
-    }
-    public void settradeArea(String ta){
-        tradeArea=ta;
-    }
-    public void setHotelLocation(String hl){
-        hotelLocation=hl;
-    }
-    public void setHotelPhone(int[] hp){
-        hotelPhone=hp;
-    }
-    public void setStars(int st){
-        stars=st;
-    }
-    public void setInstruction(String in){
-        instruction=in;
-    }
-    public void setEvaluation(Evaluation eva){
-        eval.add(eva);
-    }
-
-    public String getPassword(){
-        return password;
-    }
-    public String getHotelName(){
-        return hotelName;
-    }
-    public String gettradeArea(){
-        return tradeArea;
-    }
-    public String getHotelLocation(){
-        return hotelLocation;
-    }
-    public int[] getHotelPhone(){
-        return hotelPhone;
-    }
-    public int getStars(){
-        return stars;
-    }
-    public String getInstruction(){
-        return instruction;
-    }
-    public ArrayList getEvaluation(){
-        return eval;
-    }
+public class HotelPO {
+	int id;
+	String password;//密码
+	String hotelName;//酒店名称
+	String tradeArea;//所属商圈
+	String hotelLocation;//酒店地址
+	String hotelPhone;//酒店联系电话
+	String stars;//酒店星级
+	String instruction;//酒店简介
+	ArrayList<Evaluation> eval;//酒店评价
+	String cooperatateCompany;//酒店合作企业
+	public HotelPO(int i,String hn,String ta,String hl,String hp,String st,String in,Evaluation eva,String cc){
+		id=i;
+		hotelName=hn;
+		tradeArea=ta;
+		hotelLocation=hl;
+		hotelPhone=hp;
+		stars=st;
+		instruction=in;
+		eval.add(eva);
+		cooperatateCompany=cc;
+	}
+	public void setID(int i){
+		id=i;
+	}
+	public void setPassword(String pw){
+		password=pw;
+	}
+	public void setHotelName(String hn){
+		hotelName=hn;
+	}
+	public void settradeArea(String ta){
+		tradeArea=ta;
+	}
+	public void setHotelLocation(String hl){
+		hotelLocation=hl;
+	}
+	public void setHotelPhone(String hp){
+		hotelPhone=hp;
+	}
+	public void setStars(String st){
+		stars=st;
+	}
+	public void setInstruction(String in){
+		instruction=in;
+	}
+	public void setEvaluation(ArrayList arrayList){
+	
+	}
+	public void setCooperatateCompany(String str){
+		cooperatateCompany=str;
+	}
+	
+	public int getID(){
+		return id;
+	}
+	public String getPassword(){
+		return password;
+	}
+	public String getHotelName(){
+		return hotelName;
+	}
+	public String gettradeArea(){
+		return tradeArea;
+	}
+	public String getHotelLocation(){
+		return hotelLocation;
+	}
+	public String getHotelPhone(){
+		return hotelPhone;
+	}
+	public String getStars(){
+		return stars;
+	}
+	public String getInstruction(){
+		return instruction;
+	}
+	public ArrayList getEvaluation(){
+		return eval;
+	}
+	public String getCooperatateCompany(){
+		return cooperatateCompany;
+	}
 }
