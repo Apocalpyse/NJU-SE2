86c9b633df78221d222abc2e2a64f563c378317b
chenjin163 <13951723565@163.com>
2016-12-04 22:06:08 +0800

    Update RoomDataService.java

--- Client/src/dataservice/roomdataservice/RoomDataService.java
+++ Client/src/dataservice/roomdataservice/RoomDataService.java
@@ -3,5 +3,16 @@ package dataservice.roomdataservice;
 /**
  * Created by 常德隆 on 2016/11/20.
  */
+import po.HotelPO;
+import po.RoomPO;
+
 public class RoomDataService {
+    public RoomPO find(String roomStyle){
+		return null;
+	}
+    public void updata(RoomPO roompo){
+		
+	}
+	
 }
+
