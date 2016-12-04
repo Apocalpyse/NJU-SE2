75a35203b8de85d20b6f0e93019c82799c5911ea
chenjin163 <13951723565@163.com>
2016-12-04 22:05:30 +0800

    Update HotelDataService.java

--- Client/src/dataservice/hoteldataservice/HotelDataService.java
+++ Client/src/dataservice/hoteldataservice/HotelDataService.java
@@ -1,13 +1,13 @@
 package dataservice.hoteldataservice;
 
+import dataservice.roomdataservice.RoomDataService;
 import po.HotelPO;
 
-/**
- * Created by 常德隆 on 2016/11/20.
- */
 public class HotelDataService {
-    public HotelPO find(long id){
-        HotelPO po=new HotelPO();
-        return po;
-    }
+	public HotelPO find(long id){
+		return null;
+	}
+	public void updata(HotelPO hotelpo){
+		
+	}
 }
