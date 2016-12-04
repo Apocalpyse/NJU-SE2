939f69a07b8f1c959103e23f2db966267f5059db
chenjin163 <13951723565@163.com>
2016-12-04 21:47:25 +0800

    Update RoomVO.java

--- Client/src/vo/RoomVO.java
+++ Client/src/vo/RoomVO.java
@@ -3,5 +3,35 @@ package vo;
 /**
  * Created by 常德隆 on 2016/11/19.
  */
-public class RoomVO {
+//图片
+	String roomType;//房间类型
+	int roomTotalNumber;//房间总数量
+	int roomAccessNumber;//可预定的房间数量
+	double roomPrice;//房间价格
+		
+	public void setRoomType(String st){
+		roomType=st;
+	}
+	public void setRoomTotalNumber(int rtn){
+		roomTotalNumber=rtn;
+	}
+	public void setRoomAccessNumber(int ran){
+		roomAccessNumber=ran;
+	}
+	public void setRoomPrice(double rp){
+		roomPrice=rp;
+	}
+		
+	public String getRoomType(){
+		return roomType;
+	}
+	public int getRoomTotalNumber(){
+		return roomTotalNumber;
+	}
+	public int getRoomAccessNumber(){
+		return roomAccessNumber;
+	}
+	public double getRoomPrice(){
+		return roomPrice;
+	}	
 }
