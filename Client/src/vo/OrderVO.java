654e4edce2efafddda1b72c3d0ecc0de15a1e94a
chenjin163 <13951723565@163.com>
2016-12-10 15:50:54 +0800

    Update OrderVO.java

--- Client/src/vo/OrderVO.java
+++ Client/src/vo/OrderVO.java
@@ -1,144 +1,194 @@
-package vo;
-
 import po.OrderState;
 
 /**
- * Created by 常德隆 on 2016/11/19.
+ * Created by 甯稿痉闅� on 2016/11/19.
  */
 public class OrderVO {
-    private long id;
-    private String customerName;
-    private String customerPhone;
-    private String hotelName;
-    private String hotelPhone;
-    private String hotelLocation;
-    private String roomType;
-    private int roomNumber;
-    private double roomPrice;
-    private double discount;
-    private String startTime;
-    private String endTime;
-    private double totalPrice;
-    private boolean isExistChild;
-    private int customerNumber;
+	 private long id;
+	    private String customerName;
+	    private String customerPhone;
+	    private String hotelName;
+	    private String hotelPhone;
+	    private String hotelLocation;
+	    private String roomType;
+	    private int roomNumber;
+	    private double roomPrice;
+	    private double discount;
+	    private String startTime;
+	    private String endTime;
+	    private String executeTime;
+	    private double totalPrice;
+	    private OrderState os;
+	    private boolean isExistChild;
+	    private int customerNumber;
+	    private long masterId;
 
-    public long getId(){
-        return id;
-    }
+	    public OrderVO(){}
 
-    public void setId(long id){
-        this.id=id;
-    }
+	    public OrderVO(long id,String customerName,String customerPhone,String hotelName,String hotelPhone,
+	                   String hotelLocation,String roomType,int roomNumber,double roomPrice,
+	                   double discount,String startTime,String endTime,String executeTime,double totalPrice,
+	                   OrderState os,boolean isExistChild,int customerNumber){
+	        this.id=id;
+	        this.customerName=customerName;
+	        this.customerPhone=customerPhone;
+	        this.hotelName=hotelName;
+	        this.hotelPhone=hotelPhone;
+	        this.hotelLocation=hotelLocation;
+	        this.roomType=roomType;
+	        this.roomNumber=roomNumber;
+	        this.roomPrice=roomPrice;
+	        this.discount=discount;
+	        this.startTime=startTime;
+	        this.endTime=endTime;
+	        this.executeTime=executeTime;
+	        this.totalPrice=totalPrice;
+	        this.os=os;
+	        this.isExistChild=isExistChild;
+	        this.customerNumber=customerNumber;
+	    }
 
-    public String getCustomerName() {
-        return customerName;
-    }
+	    public long getId(){
+	        return id;
+	    }
 
-    public void setCustomerName(String name){
-        this.customerName=name;
-    }
+	    public void setId(long id){
+	        this.id=id;
+	    }
 
-    public String getCustomerPhone(){
-        return customerPhone;
-    }
+	    public String getCustomerName() {
+	        return customerName;
+	    }
 
-    public void setCustomerPhone(String phone){
-        this.customerPhone=phone;
-    }
+	    public void setCustomerName(String name){
+	        this.customerName=name;
+	    }
 
-    public String getHotelName(){
-        return hotelName;
-    }
+	    public String getCustomerPhone(){
+	        return customerPhone;
+	    }
 
-    public void setHotelName(String hotelName) {
-        this.hotelName = hotelName;
-    }
+	    public void setCustomerPhone(String phone){
+	        this.customerPhone=phone;
+	    }
 
-    public String getHotelPhone() {
-        return hotelPhone;
-    }
+	    public String getHotelName(){
+	        return hotelName;
+	    }
 
-    public void setHotelPhone(String hotelPhone) {
-        this.hotelPhone = hotelPhone;
-    }
+	    public void setHotelName(String hotelName) {
+	        this.hotelName = hotelName;
+	    }
 
-    public String getHotelLocation() {
-        return hotelLocation;
-    }
+	    public String getHotelPhone() {
+	        return hotelPhone;
+	    }
 
-    public void setHotelLocation(String hotelLocation) {
-        this.hotelLocation = hotelLocation;
-    }
+	    public void setHotelPhone(String hotelPhone) {
+	        this.hotelPhone = hotelPhone;
+	    }
 
-    public String getRoomType() {
-        return roomType;
-    }
+	    public String getHotelLocation() {
+	        return hotelLocation;
+	    }
 
-    public void setRoomType(String roomType) {
-        this.roomType = roomType;
-    }
+	    public void setHotelLocation(String hotelLocation) {
+	        this.hotelLocation = hotelLocation;
+	    }
 
-    public int getRoomNumber() {
-        return roomNumber;
-    }
+	    public String getRoomType() {
+	        return roomType;
+	    }
 
-    public void setRoomNumber(int roomNumber) {
-        this.roomNumber = roomNumber;
-    }
+	    public void setRoomType(String roomType) {
+	        this.roomType = roomType;
+	    }
 
-    public double getRoomPrice() {
-        return roomPrice;
-    }
+	    public int getRoomNumber() {
+	        return roomNumber;
+	    }
 
-    public void setRoomPrice(double roomPrice) {
-        this.roomPrice = roomPrice;
-    }
+	    public void setRoomNumber(int roomNumber) {
+	        this.roomNumber = roomNumber;
+	    }
 
-    public double getDiscount() {
-        return discount;
-    }
+	    public double getRoomPrice() {
+	        return roomPrice;
+	    }
 
-    public void setDiscount(double discount) {
-        this.discount = discount;
-    }
+	    public void setRoomPrice(double roomPrice) {
+	        this.roomPrice = roomPrice;
+	    }
 
-    public String getStartTime() {
-        return startTime;
-    }
+	    public double getDiscount() {
+	        return discount;
+	    }
 
-    public void setStartTime(String startTime) {
-        this.startTime = startTime;
-    }
+	    public void setDiscount(double discount) {
+	        this.discount = discount;
+	    }
 
-    public String getEndTime() {
-        return endTime;
-    }
+	    public String getStartTime() {
+	        return startTime;
+	    }
 
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
+	    public void setStartTime(String startTime) {
+	        this.startTime = startTime;
+	    }
 
-    public double getTotalPrice() {
-        return totalPrice;
-    }
+	    public String getEndTime() {
+	        return endTime;
+	    }
 
-    public void setTotalPrice(double totalPrice) {
-        this.totalPrice = totalPrice;
-    }
+	    public void setEndTime(String endTime) {
+	        this.endTime = endTime;
+	    }
 
-    public boolean getIsExistChild() {
-        return isExistChild;
-    }
+	    public String getExecuteTime() {
+	        return executeTime;
+	    }
 
-    public void setIsExistChild(boolean isExistChild) {
-        this.isExistChild = isExistChild;
-    }
+	    public void setExecuteTime(String executeTime) {
+	        this.executeTime = executeTime;
+	    }
 
-    public int getCustomerNumber() {
-        return customerNumber;
-    }
+	    public double getTotalPrice() {
+	        return totalPrice;
+	    }
 
-    public void setCustomerNumber(int customerNumber) {
-        this.customerNumber = customerNumber;
-    }
+	    public void setTotalPrice(double totalPrice) {
+	        this.totalPrice = totalPrice;
+	    }
+
+	    public OrderState getOs() {
+	        return os;
+	    }
+
+	    public void setOs(OrderState os) {
+	        this.os = os;
+	    }
+
+	    public boolean getIsExistChild() {
+	        return isExistChild;
+	    }
+
+	    public void setIsExistChild(boolean isExistChild) {
+	        this.isExistChild = isExistChild;
+	    }
+
+	    public int getCustomerNumber() {
+	        return customerNumber;
+	    }
+
+	    public void setCustomerNumber(int customerNumber) {
+	        this.customerNumber = customerNumber;
+	    }
+
+	    public long getMasterId() {
+	        return masterId;
+	    }
+
+	    public void setMasterId(long masterId) {
+	        this.masterId = masterId;
+	    }
 }
