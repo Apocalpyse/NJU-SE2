bd1daf00681c25c70b87a07a4207fe9093bf97ee
CHR <286674013@qq.com>
2016-12-10 15:15:49 +0800

    update PO

--- Server/src/po/WebworkerPO.java
+++ Server/src/po/WebworkerPO.java
@@ -1,43 +1,51 @@
 package po;
 
+
+import po.Authority;
+
 public class WebworkerPO {
-		private long ID;
-		private String webworkerName;
-		private Authority authority;//权限
-		private String password;
-		//constructor
-		public WebworkerPO(){
-			
-		}
-		public WebworkerPO(long ID,String webworkerName,Authority authority,String password){
-			this.ID=ID;
-			this.webworkerName=webworkerName;
-			this.authority=authority;
-			this.password=password;
-		}
-		public long getID() {
-			return ID;
-		}
-		public void setID(long ID) {
-			this.ID = ID;
-		}//need to think;
-		public String getWebworkerName() {
-			return webworkerName;
-		}
-		public void setWebworkerName(String webworkerName) {
-			this.webworkerName = webworkerName;
-		}
-		public Authority getAuthority() {
-			return authority;
-		}
-		public void setAuthority(Authority authority) {
-			this.authority = authority;
-		}
-		public String getPassword() {
-			return password;
-		}
-		public void setPassword(String password) {
-			this.password = password;
-		}
+	private long ID;
+	private String webworkerName;
+	private String webworkerPhone;//电话
+	private Authority authority;//权限
+	//constructor
+	public WebworkerPO(){
 		
+	}
+	
+	public WebworkerPO(long iD, String webworkerName, String webworkerPhone, Authority authority) {
+		ID = iD;
+		this.webworkerName = webworkerName;
+		this.webworkerPhone = webworkerPhone;
+		this.authority = authority;
+	}
+
+	public String getWebworkerPhone() {
+		return webworkerPhone;
+	}
+
+	public void setWebworkerPhone(String webworkerPhone) {
+		this.webworkerPhone = webworkerPhone;
+	}
+
+	public long getID() {
+		return ID;
+	}
+	public void setID(long ID) {
+		this.ID = ID;
+	}//need to think;
+	public String getWebworkerName() {
+		return webworkerName;
+	}
+	public void setWebworkerName(String webworkerName) {
+		this.webworkerName = webworkerName;
+	}
+	public Authority getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(Authority authority) {
+		this.authority = authority;
+	}
+
 }
