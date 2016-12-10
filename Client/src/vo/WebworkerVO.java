406bbd357f8cb7bfaa061961e3667a6e056c7af9
CHR <286674013@qq.com>
2016-12-10 15:37:17 +0800

    PO VO

--- Client/src/vo/WebworkerVO.java
+++ Client/src/vo/WebworkerVO.java
@@ -5,23 +5,35 @@ import po.Authority;
 public class WebworkerVO {
 	private long ID;
 	private String webworkerName;
+	private String webworkerPhone;//电话
 	private Authority authority;//权限
 	//constructor
 	public WebworkerVO(){
 		
 	}
-	public WebworkerVO(long iD, String webworkerName, Authority authority) {
+	
+	public WebworkerVO(long iD, String webworkerName, String webworkerPhone, Authority authority) {
 		super();
 		ID = iD;
 		this.webworkerName = webworkerName;
+		this.webworkerPhone = webworkerPhone;
 		this.authority = authority;
 	}
+
+	public String getWebworkerPhone() {
+		return webworkerPhone;
+	}
+
+	public void setWebworkerPhone(String webworkerPhone) {
+		this.webworkerPhone = webworkerPhone;
+	}
+
 	public long getID() {
 		return ID;
 	}
 	public void setID(long ID) {
 		this.ID = ID;
-	}//need to think;
+	}
 	public String getWebworkerName() {
 		return webworkerName;
 	}
