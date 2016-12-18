403af83b1288787d979a8839ca45a9a86fddec90
chenjin163 <13951723565@163.com>
2016-12-18 20:00:48 +0800

    Update HotelBusinessLogicService.java

--- Client/src/businesslogicservice/hotelbusinesslogicservice/HotelBusinessLogicService.java
+++ Client/src/businesslogicservice/hotelbusinesslogicservice/HotelBusinessLogicService.java
@@ -1,5 +1,6 @@
 package businesslogicservice.hotelbusinesslogicservice;
 
+import po.Evaluation;
 import po.OrderState;
 import vo.HotelVO;
 
@@ -12,10 +13,12 @@ import java.util.ArrayList;
 public interface HotelBusinessLogicService {
      HotelVO getHotel(long id)throws RemoteException;
      boolean changeHotel(HotelVO hv)throws RemoteException;
-     boolean changePassWord(long id,String pw)throws RemoteException;
+     boolean changeGoal(HotelVO hv,Evaluation eva)throws RemoteException;
+     boolean changePassWord(String account,String pw)throws RemoteException;
      boolean addEvaluation(long id,ArrayList evaluation)throws RemoteException;
-     boolean changeOrderState(long id, OrderState os)throws RemoteException;
-     boolean setPromotionOne(double[] numberOfRoom,double[] discount)throws RemoteException;
-     boolean setPromotionTwo(double discount)throws RemoteException;
-     boolean setPromotionThree(String beginTime,String endTime)throws RemoteException;
+     boolean changeOraderState(long id,OrderState os)throws RemoteException;
+     boolean setPromotionOne(long id,int numberOfRoom,double discount)throws RemoteException;
+     boolean setPromotionTwo(long id,double discount)throws RemoteException;
+     boolean setPromotionThree(long id,String beginTime,String endTime)throws RemoteException;
+     boolean setPromotionFour(long id,double discount)throws RemoteException;
 }
