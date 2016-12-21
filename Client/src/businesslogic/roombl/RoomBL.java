17e99cdeca5d10c44316439087cb7ab1569a1a8e
chenjin163 <13951723565@163.com>
2016-12-22 02:01:16 +0800

    Update RoomBL.java

--- Client/src/businesslogic/roombl/RoomBL.java
+++ Client/src/businesslogic/roombl/RoomBL.java
@@ -1,37 +1,46 @@
 package businesslogic.roombl;
 
+import java.rmi.RemoteException;
+
 import businesslogicservice.roombusinesslogicservice.RoomBusinessLogicService;
+import dataservice.hoteldataservice.HotelDataService;
 import dataservice.roomdataservice.RoomDataService;
-import dataservice.roomdataservice.RoomFactory;
+import po.HotelPO;
 import po.RoomPO;
 import vo.RoomVO;
 
-import java.net.MalformedURLException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 public class RoomBL implements RoomBusinessLogicService{
 
-	private RoomDataService rds;
-
-    public RoomBL() throws RemoteException{
-        try {
-            RoomFactory roomFactory=(RoomFactory) Naming.lookup("rmi://127.0.0.1:1234/roomFactory");
-            this.rds=roomFactory.createRoomDataService();
-        }catch (MalformedURLException e) {
-            e.printStackTrace();
-        }catch (NotBoundException e) {
-            e.printStackTrace();
+	RoomDataService rds=new RoomDataService() {
+        @Override
+        public RoomPO find(long id) {
+            return null;
         }
-    }
 
-	public RoomVO getRoom(long roomStyle){
+        @Override
+        public boolean insert(RoomPO po) {
+            return false;
+        }
+
+        @Override
+        public boolean delete(long id) {
+            return false;
+        }
+
+        @Override
+        public boolean update(RoomPO po) {
+            return false;
+        }
+
+	
+    };
+
+	public RoomVO getRoom(long roomStyle)throws RemoteException{
 		RoomPO rpo=this.rds.find(roomStyle);
 		return new RoomVO(rpo.getRoomID(),rpo.getRoomType(),rpo.getRoomTotalNumber(),rpo.getRoomAccessNumber(),
 				rpo.getRoomPrice());
 	}
-	public boolean changeRoom(RoomVO rv){
+	public boolean changeRoom(RoomVO rv)throws RemoteException{
 		RoomPO rpo=this.rds.find(rv.getRoomID());
 		return rds.update(new RoomPO(rv.getRoomID(),rv.getRoomType(),rv.getRoomTotalNumber(),
 				rv.getRoomAccessNumber(),rv.getRoomPrice()));
